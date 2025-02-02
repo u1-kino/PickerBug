009dde3ecea1b493c00360e2d2c025d8b412166d
Yuichi Kinoshita <u1.kino@gmail.com>
2025-02-02 10:41:08 +0900

    first commit

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	List<string> items = new List<string>
+	{
+		"1: First", "2: Second", "3: Third", "4: Fourth", "5: Fifth" , 
+		"6: Sixth", "7: Seventh", "8: Eighth", "9: Ninth", "10: Tenth"
+	};
+
+    public List<string> PickerItems { get; set; }
+	public string? SelectedItem { get; set; }
+
+	public MainPage()
+	{
+		InitializeComponent();
+		
+        PickerItems = items;
+
+		SelectedItem = items[4]; // "Fifth" Selected
+
+        BindingContext = this;
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+
+    private void OnPickerSelectedIndexChanged(object sender, EventArgs e)
+    {
+        var picker = (Picker)sender;
+        SelectedItem = (string)picker.SelectedItem;
+        // Add other processing based on the selected item as needed
+    }
+}
+
---
+++ MauiApp1/MauiProgram.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MauiApp1;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			})
+			.ConfigureMauiHandlers((handlers) =>
+			{
+#if MACCATALYST
+				// Uncomment this handler
+				// Workaround for Picker not updating SelectedItem on Mac Catalyst 
+				handlers.AddHandler(typeof(Picker), typeof(FixPickerHandler));
+#endif
+			}
+			);
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
